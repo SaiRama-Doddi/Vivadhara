d53f9352d3c24ab7d69b02a8d22548a4a347c2fe
Sai Rama Doddi <sairama1026@gmail.com>
2025-11-29 21:31:02 +0530

    added text title

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -38,9 +38,10 @@ export default function Navbar({ onNavigate }: NavbarProps) {
               alt="Logo"
               className="w-18 h-18 rounded-full shadow-xl object-cover"
             />
-            <span className="text-2xl md:text-2xl sm:text-xl font-semibold text-white uppercase">
-              Vivadhara Agro
-            </span>
+           <span className="text-xl sm:text-2xl md:text-3xl font-semibold text-white uppercase">
+  Vivadhara Agro
+</span>
+
           </div>
 
           {/* DESKTOP NAV */}
